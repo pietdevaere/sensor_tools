405af449df305e936c51dee21bbd7234db18006a
Piet De Vaere <piet@devae.re>
2018-04-30 14:21:15 +0200

    split adding and transmitting measurement

--- sensing_tools.py
+++ sensing_tools.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from influxdb import InfluxDBClient
 
@@ -10,7 +11,6 @@ class InfluxReporter:
     def __init__(self, secret_file_path):
         self.config = read_secrets(secret_file_path)['influx']
 
-
         self.client = InfluxDBClient(   self.config['host'],
                                         self.config['port'],
                                         self.config['user'],
@@ -19,8 +19,10 @@ class InfluxReporter:
                                         ssl = self.config['ssl'],
                                         verify_ssl = True)
 
-    def report(self, measurement, sensor, location, value):
-        json_body = [ { "measurement": measurement,
+        self.buffer = list()
+
+    def add_measurement(self, measurement, sensor, location, value):
+        buffer_entry = { "measurement": measurement,
                         "tags": {
                             "sensor" :   sensor,
                             "location" : location,
@@ -28,10 +30,17 @@ class InfluxReporter:
                         "fields" : {
                             "value": value,
                             },
-                        } ]
+                        "time" : time.time()
+                        }
+        self.buffer.append(buffer_entry)
+
+    def transmit(self):
         self.client.write_points(json_body)
 
 
+    def report(self, measurement, sensor, location, value):
+        self.add_measurement(measurement, sensor, location, value)
+        self.transmit()
 
 
 if __name__ == "__main__":
